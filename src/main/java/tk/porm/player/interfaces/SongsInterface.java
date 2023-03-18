df884a95386d5b44ba2ccf97b20078bd3176118f
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-03-18 20:23:54 +0800

    chore: remove extra space

--- src/main/java/tk/porm/player/interfaces/SongsInterface.java
+++ src/main/java/tk/porm/player/interfaces/SongsInterface.java
@@ -6,5 +6,5 @@ import tk.porm.player.objects.Song;
 public interface SongsInterface {
 	public ArrayList<Song> getSongs(String search);
 	public void addSong(String location, String title, String artist);
-	public void removeSong (int id);
+	public void removeSong(int id);
 }
