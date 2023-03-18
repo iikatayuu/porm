eddf674932c2e0614b43e4ec5ee89920d63f64b0
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-03-18 20:17:43 +0800

    perf: remove abstract

--- src/main/java/tk/porm/player/interfaces/PlayerListener.java
+++ src/main/java/tk/porm/player/interfaces/PlayerListener.java
@@ -1,7 +1,7 @@
 package tk.porm.player.interfaces;
 
 public interface PlayerListener {
-	public abstract void progress(int read, int length);
-	public abstract void onStart();
-	public abstract void onStop();
+	public void progress(int read, int length);
+	public void onStart();
+	public void onStop();
 }
--- src/main/java/tk/porm/player/interfaces/StreamListener.java
+++ src/main/java/tk/porm/player/interfaces/StreamListener.java
@@ -1,6 +1,6 @@
 package tk.porm.player.interfaces;
 
 public interface StreamListener {
-	public abstract void progress(int read, int length);
-	public abstract void beforeClose();
+	public void progress(int read, int length);
+	public void beforeClose();
 }
