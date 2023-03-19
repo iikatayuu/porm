ab87910e167a7351b373d1784e9e04712c702247
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-03-19 15:32:21 +0800

    perf: update database structure

--- setup/database.sql
+++ setup/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 17, 2023 at 11:49 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Generation Time: Mar 19, 2023 at 07:31 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `porm`
 --
+CREATE DATABASE IF NOT EXISTS `porm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `porm`;
 
 -- --------------------------------------------------------
 
@@ -34,17 +36,17 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `value` text NOT NULL COMMENT 'Settings value',
   `last` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Last update',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `settings`
 --
 
 INSERT INTO `settings` (`id`, `name`, `value`, `last`) VALUES
-(1, 'theme', 'dark', '2023-03-18 00:00:00'),
-(2, 'repeat', 'none', '2023-03-18 00:00:00'),
-(3, 'shuffle', 'false', '2023-03-18 00:00:00'),
-(4, 'compact', 'false', '2023-03-19 00:00:00');
+(1, 'theme', 'dark', '2023-03-18 12:22:02'),
+(2, 'repeat', 'all', '2023-03-18 15:25:17'),
+(3, 'shuffle', 'true', '2023-03-18 15:25:24'),
+(4, 'compact', 'false', '2023-03-19 05:24:54');
 
 -- --------------------------------------------------------
 
@@ -55,10 +57,11 @@ INSERT INTO `settings` (`id`, `name`, `value`, `last`) VALUES
 DROP TABLE IF EXISTS `songs`;
 CREATE TABLE IF NOT EXISTS `songs` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Song unique ID',
-  `location` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Song file location',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Song title',
   `artist` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Song artist',
-  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Song added',
+  `liked` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Song was liked',
+  `location` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'MP3 location',
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Added date',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
